6fd02f3b65dbdf7e690f5f489fb6743c0c9a94eb
Adrianne Lois Dela Cruz <108196325+adrianneloisdelacruz@users.noreply.github.com>
2024-07-24 18:37:10 +0800

    Update Introduction to Transact-SQL.sql

--- Introduction to Transact-SQL.sql
+++ Introduction to Transact-SQL.sql
@@ -10,7 +10,14 @@ Where I learn what kinds of statements make up the SQL language and look at the 
 */
 
 /*Challenge 1: Retrieve customer data
-1.1 Retrieve customer details
+1.1 Retrieve customer details:
 Familiarize yourself with the SalesLT.Customer table by writing a Transact-SQL query that retrieves all columns for all customers.*/
 
 SELECT * FROM SalesLT.Customer;
+
+/*
+1.2 Retrieve customer name data:
+*/
+  
+SELECT Title, FirstName, MiddleName, LastName, Suffix
+FROM SalesLT.Customer;
